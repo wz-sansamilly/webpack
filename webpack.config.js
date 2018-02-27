34ae8a199b522b37242989efb414b0b9503ef587
wxj <sansamilly@sina.com>
2018-02-27 18:01:54 +0800

    css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,27 @@ module.exports = {
     port:7070,// 端口
     inline:true,// 源文件改变时页面自动刷新
     historyApiFallback:true// 依赖于H5 history API,true-所有的跳转都指向index页面
+  },
+  module:{
+    rules:[
+      {
+        test: /(\.jsx|\.js)$/,// 匹配loaders所处理文件的扩展名的正则表达式
+        use:{
+          loader:"babel-loader",
+        },
+        exclude:/node_modules/ // 屏蔽不需要处理的文件 include-手动添加必须处理的文件
+      },
+      {
+        test:/\.css$/,
+        use:[
+          {
+            loader:'css-loader'
+          },
+          {
+            loader:'style-loader'
+          }
+        ]
+      }
+    ]
   }
 }
